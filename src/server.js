1588702606baa034d1be6d0fe64c3579cc0b31b4
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2023-06-11 13:06:27 +0200

    update

--- src/server.js
+++ src/server.js
@@ -58,11 +58,12 @@ app.use(/.*-[0-9a-f]{10}\..*/, (req, res, next) => {
 
 
 
-
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use("/", express.static("public"));
 app.use('/', express.static('src/static'));
 app.use('/', express.static('src/public'));
 
@@ -80,12 +81,35 @@ const njk = expressNunjucks(app, {
 // });
 
 app.get("/", HomeController);
-app.get("/:q", SearchController);
+app.get("/search/:q", SearchController);
 
 app.get("/collection", CollectionController);
 app.get("/collection/:id", CollectionDetailsController);
+
+
+app.get('*', function (req, res, next) {
+	let err = new Error(`${req.ip} tried to reach ${req.originalUrl}`); // Tells us which IP tried to reach a particular URL
+	err.statusCode = 404;
+	err.shouldRedirect = true; //New property on err so that our middleware will redirect
+	next(err);
+});
+
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.render('error.njk', {
+		title: error,
+		error
+	});
+});
+
 
 ViteExpress.listen(app, PORT, () => {
   console.log(__dirname)
   console.log("Server is listening on port 3000...")
 });
+
+
+// app.listen(PORT, () => {
+//   console.log(__dirname);
+//   console.log(`Server is listening on port ${PORT}...`);
+// });
